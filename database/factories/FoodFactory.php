13a6889801f0367bf43b096f81c88bcb2a14f01a
Datvt <datvt@hblab,vn>
2022-07-15 10:19:03 +0700

    fix food api

--- database/factories/FoodFactory.php
+++ database/factories/FoodFactory.php
@@ -19,7 +19,6 @@ class FoodFactory extends Factory
             'store_id' => array_rand($storeIds),
             'name' => $this->faker->name(),
             'price' => $this->faker->randomDigit(),
-            'image' => $this->faker->imageUrl($width = 200, $height = 200),
             'description' => $this->faker->text(),
         ];
     }
