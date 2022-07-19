52434d210fbb58091214efd52bab174c286661aa
Datvt <datvt@hblab,vn>
2022-07-19 13:48:22 +0700

    update api

--- app/Http/Controllers/Api/V1/CastController.php
+++ app/Http/Controllers/Api/V1/CastController.php
@@ -78,7 +78,7 @@ class CastController extends Controller
      */
     public function update(UpdateCastRequest $request, Cast $cast): JsonResponse
     {
-        if ($updateRecord = $this->castService->update($request->validated(), $cast)) {
+        if ($updateRecord = $this->castService->update($request->all(), $cast)) {
             return response()->json([
                 'message' => MessageStatus::SUCCESS,
                 'data' => $updateRecord
